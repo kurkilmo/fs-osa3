61b8942a2eabcb510adf05d452564826f64cbee3
kurkilmo <ilmo.kurki00@gmail.com>
2024-05-04 14:57:29 +0300

    Tehtävä 3.14

--- index.js
+++ index.js
@@ -59,16 +59,12 @@ app.get('/api/persons', (req, resp) => {
     Person.find({}).then(people => {
         resp.json(people)
     })
-    //resp.json(persons)
 })
 
 app.get('/api/persons/:id', (req, resp) => {
     Person.findById(req.params.id).then(person => {
         resp.json(person)
     })
-    //const person = persons.find(p => p.id === id)
-    //if (person) resp.json(person)
-    //else resp.status(404).end()
 })
 
 app.delete('/api/persons/:id', (req, resp) => {
@@ -85,18 +81,13 @@ app.post('/api/persons', (req, resp) => {
     const body = req.body
     if (!body.name) return resp.status(400).json({"error":"name missing"})
     if (!body.number) return resp.status(400).json({"error":"number missing"})
-    if (persons.map(p => p.name).includes(body.name)) {
-        return resp.status(403).json({"error":"name must be unique"})
-    }
-    
-    const id = Math.floor(Math.random() * 2**64) // mikä homma :D
-    const newPerson = {
-        "id": id,
+    const person = new Person({
         "name": body.name,
         "number": body.number
-    }
-    persons = persons.concat(newPerson)
-    resp.json(newPerson)
+    })
+    person.save().then(saved => {
+        resp.json(saved)
+    })
 })
 
 const PORT = process.env.PORT || 8080
